1ae98469662cd79ebb17c256832acb5cfc31dcc9
Elizabeth <mwendemuthusi6@gmail.com>
2023-02-11 15:31:04 +0300

    Completed assignment

--- countdown.rb
+++ countdown.rb
@@ -1,4 +1,17 @@
 #write your code here
 
-def countdown
+def countdown number
+  i= 0
+  while number>i
+    countdown_with_sleep(5)
+    puts "#{number} SECOND(S)!"
+    number-=1
+  end
+
+  "HAPPY NEW YEAR!"
 end
+
+def countdown_with_sleep(time)
+  sleep(time)
+end
+puts countdown 8
